79ae6419f0cd4866f6add6bcf92a9bdf1779a4d8
David Fearon <david@davidfearon.com>
2022-12-19 16:25:14 +0000

    Initial Commit

---
+++ EssTeeEll/EssTeeEllApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssTeeEllApp.swift
+//  EssTeeEll
+//
+//  Created by David Fearon on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct EssTeeEllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
